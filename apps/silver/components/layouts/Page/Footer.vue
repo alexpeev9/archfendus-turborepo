eaa667d642d3f16489eb0fdd7f024fcfbf309bc9
alexpeev9 <alexpeev8@gmail.com>
2025-10-26 01:56:05 +0300

    Add working silver site and align color variables

---
+++ apps/silver/components/layouts/Page/Footer.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import { Footer } from "ui";
+import strings from "~/assets/strings.json";
+</script>
+<template>
+  <Footer :title="strings.title" />
+</template>
--- packages/nuxt-base/components/girl-data-list.vue
+++ packages/nuxt-base/components/girl-data-list.vue
@@ -11,9 +11,13 @@ const { data: girls, pending, error } = await useGetGirls(props.site);
 </script>
 
 <template>
-  <div v-if="pending" class="loading">Loading lonely women...</div>
-  <div v-else-if="error" class="error">
-    Sorry, we couldn't load the lonely women. Please try again later.
+  <div v-if="pending" class="collections">
+    <h2 class="section-header">Loading lonely women...</h2>
+  </div>
+  <div v-else-if="error" class="collections">
+    <h2 class="section-header">
+      Sorry, we couldn't load the lonely women. Please try again later.
+    </h2>
   </div>
   <GirlList v-else-if="girls">
     <GirlCard v-for="girl in girls" :key="girl.name" :girl="girl" />
