a7d7464eae66e437213865d15188ad7ef8c80fe9
alexpeev9 <alexpeev8@gmail.com>
2025-10-26 11:23:56 +0200

    Fix file name

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,5 +1,5 @@
-import GirlCard from "./components/girls-list/girl-card.vue";
-import GirlList from "./components/girls-list/girl-list.vue";
+import GirlCard from "./components/girl-list/girl-card.vue";
+import GirlList from "./components/girl-list/girl-list.vue";
 import Hero from "./components/landing/hero.vue";
 import Footer from "./components/layout/footer.vue";
 import Header from "./components/layout/header.vue";
